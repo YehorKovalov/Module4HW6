32cff3498b78c214cfc7d0f0cf23bc66f54b9602
MascotYehorKovalov <yehor.kovalov1@nure.ua>
2022-01-29 13:45:58 +0200

    Entities, Relationships between entities, entities' configurations are added

---
+++ SongLibrary/SongLibrary/Domain/EntitiesConfigurations/GenreConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SongLibrary.Domain.Entities;
+
+namespace SongLibrary.Domain.EntitiesConfigurations
+{
+    public class GenreConfiguration : IEntityTypeConfiguration<GenreEntity>
+    {
+        public void Configure(EntityTypeBuilder<GenreEntity> builder)
+        {
+            builder.ToTable("Genre").HasKey(g => g.GenreId);
+            builder.Property(g => g.GenreId).ValueGeneratedOnAdd();
+            builder.Property(g => g.Title).IsRequired().HasMaxLength(50);
+        }
+    }
+}
---
+++ SongLibrary/SongLibrary/Domain/EntitiesConfigurations/SongConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SongLibrary.Domain.Entities;
+
+namespace SongLibrary.Domain.EntitiesConfigurations
+{
+    public class SongConfiguration : IEntityTypeConfiguration<SongEntity>
+    {
+        public void Configure(EntityTypeBuilder<SongEntity> builder)
+        {
+            builder.ToTable("Song").HasKey(s => s.SongId);
+            builder.Property(s => s.SongId).ValueGeneratedOnAdd();
+            builder.Property(s => s.ReleasedDate).IsRequired();
+            builder.Property(s => s.Title).IsRequired().HasMaxLength(100);
+            builder.Property(s => s.Duration).IsRequired();
+
+            builder.HasOne(s => s.Genre)
+                .WithMany(g => g.Songs)
+                .HasForeignKey(s => s.GenreId)
+                .OnDelete(DeleteBehavior.ClientSetNull);
+        }
+    }
+}
