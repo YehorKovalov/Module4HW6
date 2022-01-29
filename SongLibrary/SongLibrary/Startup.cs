1e46a5050c07e1cd3a012edc28cdec06b82f587c
MascotYehorKovalov <yehor.kovalov1@nure.ua>
2022-01-29 11:33:06 +0200

    Initial application's structure

---
+++ SongLibrary/SongLibrary/Startup.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace SongLibrary
+{
+    public class Startup
+    {
+        public async Task Run()
+        {
+            var serviceProvider = ConfigureServices();
+            var app = serviceProvider?.GetService<Application>();
+            await app?.Run();
+        }
+
+        public IServiceProvider ConfigureServices()
+        {
+            var serviceCollection = new ServiceCollection();
+
+            return serviceCollection.BuildServiceProvider();
+        }
+    }
+}
