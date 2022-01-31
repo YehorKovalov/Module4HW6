756310baef9c5354dc51f5d74718bedd9360e827
MascotYehorKovalov <yehor.kovalov1@nure.ua>
2022-01-31 16:09:12 +0200

    Helpers are added: class with Safe Execution(transaction), test data seeder

---
+++ SongLibrary/SongLibrary/Helpers/BaseDataService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+using SongLibrary.Domain;
+
+namespace SongLibrary.Helpers
+{
+    public abstract class BaseDataService
+    {
+        private readonly SongLibraryDbContext _db;
+        public BaseDataService(SongLibraryDbContext db) => _db = db;
+
+        protected SongLibraryDbContext Db => _db;
+
+        public async Task<T> ExecuteSafely<T>(Func<Task<T>> action)
+        {
+            await using (var transaction = await Db.Database.BeginTransactionAsync())
+            {
+                try
+                {
+                    var result = await action.Invoke();
+                    await transaction.CommitAsync();
+                    return result;
+                }
+                catch
+                {
+                    await transaction.RollbackAsync();
+                    return default(T);
+                }
+            }
+        }
+    }
+}
---
+++ SongLibrary/SongLibrary/Helpers/TestEntitiesSeeder.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using SongLibrary.Domain;
+using SongLibrary.Domain.Entities;
+
+namespace SongLibrary.Helpers
+{
+    public class TestEntitiesSeeder
+    {
+        private const int SongsAmount = 8;
+        private const int ArtistAmount = 5;
+        private const int GenreAmount = 5;
+        private readonly SongLibraryDbContext _db;
+
+        public TestEntitiesSeeder(SongLibraryDbContext db) => _db = db;
+
+        public async Task SeedAll()
+        {
+            await SeedGenre();
+            await SeedSongsArtists();
+        }
+
+        public async Task SeedGenre()
+        {
+            var genres = new List<GenreEntity>();
+            genres.Add(new GenreEntity { Title = "Rock" });
+            genres.Add(new GenreEntity { Title = "Jazz" });
+            genres.Add(new GenreEntity { Title = "Pop music" });
+            genres.Add(new GenreEntity { Title = "Blues" });
+            genres.Add(new GenreEntity { Title = "Heavy metal" });
+            await _db.Genres.AddRangeAsync(genres);
+            await _db.SaveChangesAsync();
+        }
+
+        public async Task SeedSongsArtists()
+        {
+            var songs = GetSongs();
+            SeedSongsWithArtists(ref songs);
+
+            await _db.Songs.AddRangeAsync(songs);
+            await _db.SaveChangesAsync();
+        }
+
+        private List<SongEntity> GetSongs()
+        {
+            var songs = new List<SongEntity>();
+            for (int i = 1; i <= SongsAmount; i++)
+            {
+                songs.Add(new SongEntity
+                {
+                    Duration = GetRandomDuration(),
+                    GenreId = GetRandomGenreId(),
+                    Title = $"Song{i}",
+                    ReleasedDate = GetRandomDate()
+                });
+            }
+
+            return songs;
+        }
+
+        private List<ArtistEntity> GetArtists()
+        {
+            var artists = new List<ArtistEntity>();
+
+            for (int i = 1; i <= ArtistAmount; i++)
+            {
+                artists.Add(new ArtistEntity
+                {
+                    Name = $"Artist{i}",
+                    DateOfBirth = GetRandomDate(),
+                    Email = $"Email{i}",
+                    InstagramURL = $"instagram{i}",
+                    Phone = $"PhoneNumber{i}"
+                });
+            }
+
+            return artists;
+        }
+
+        private DateTimeOffset GetRandomDate()
+        {
+            var rand = new Random();
+            var year = rand.Next(1900, 2000);
+            var month = rand.Next(1, 12);
+            var day = rand.Next(1, 28);
+            var date = new DateTime(year, month, day);
+            return new DateTimeOffset(date);
+        }
+
+        private TimeSpan GetRandomDuration()
+        {
+            var rand = new Random();
+            var minutes = rand.Next(0, 60);
+            var seconds = rand.Next(0, 60);
+            return new TimeSpan(0, minutes, seconds);
+        }
+
+        private int GetRandomGenreId()
+        {
+            return new Random().Next(1, GenreAmount + 1);
+        }
+
+        private ArtistEntity GetRandomArtistFromList()
+        {
+            var artists = GetArtists();
+            var randomArtistIndex = new Random().Next(ArtistAmount);
+            return artists[randomArtistIndex];
+        }
+
+        private void SeedSongsWithArtists(ref List<SongEntity> songs)
+        {
+            foreach (var song in songs)
+            {
+                var artist = GetRandomArtistFromList();
+                song.Artists.Add(artist);
+            }
+        }
+    }
+}
